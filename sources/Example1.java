8b13ac3ccda7823d9b6354b61ac5d40eb009467b
Dmitry Ponyatov <dponyatov@gmail.com>
2021-08-19 10:58:29 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- sources/Example1.java
+++
@@ -1,7 +0,0 @@
-class Example1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        for (String arg: args)
-            System.out.println("\t" + arg);
-    }
-}
